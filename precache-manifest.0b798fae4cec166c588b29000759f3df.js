53c3d0ef606c84dd22d1700510715d06b0d8813d
liverwang <live_wang@live.cn>
2018-05-28 23:45:15 +0800

    Add files via upload

---
+++ precache-manifest.0b798fae4cec166c588b29000759f3df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "899bb8e38f8a41cc1f23f9537d4d7ed7",
+    "url": "/static/sw.gif"
+  },
+  {
+    "revision": "cb6cdb943911d429bdda0688c410a09b",
+    "url": "/static/skeleton.gif"
+  },
+  {
+    "revision": "1748317793fd05195ff8",
+    "url": "/static/js/vendor.17483177.js"
+  },
+  {
+    "revision": "e6d7e551acb97dfe2a6a",
+    "url": "/static/js/manifest.e6d7e551.js"
+  },
+  {
+    "revision": "66a83440c7cef4baa23b",
+    "url": "/static/js/app.66a83440.js"
+  },
+  {
+    "revision": "66a83440c7cef4baa23b",
+    "url": "/static/css/app.6821e69136a8c48aebad28cae6a5b9bc.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "6098efc7ca904587b95702ff66975958",
+    "url": "/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,24 @@
+importScripts("/precache-manifest.0b798fae4cec166c588b29000759f3df.js", "https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+
+workbox.skipWaiting()
+workbox.clientsClaim()
+
+// 设置缓存名称前缀
+workbox.core.setCacheNameDetails({
+  prefix: 'vuecli'
+})
+
+// 缓存manifest.json
+workbox.routing.registerRoute(
+  /\/manifest\.json/,
+  workbox.strategies.staleWhileRevalidate()
+)
+
+// sw-register网络请求优先
+workbox.routing.registerRoute(
+  /\/sw-register\.js/,
+  workbox.strategies.networkOnly()
+)
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
+
